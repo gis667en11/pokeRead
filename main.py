63fdb64135c7db6461a24dcc5cf7500b145ef47e
NineOCLockNate <gis667en11@gmail.com>
2022-06-02 01:31:48 -0400

    main now captures unique text boxes by hash list, appends new to list, saves screenshot by index.png

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 
 from distutils.command.build_scripts import first_line_re
+from enum import unique
+from fileinput import filename
+from numpy import append
 import pyautogui
 from PIL import Image
 import imagehash
 import pyttsx3
+engine = pyttsx3.init()
 import os
 
 # Get the directory for this program
 path_dir = os.path.dirname(os.path.realpath(__file__))
 path_Border = os.path.join(path_dir,'borderReference')
 path_processImages = os.path.join(path_dir,'processImages')
+path_uniqueDialog = os.path.join(path_dir,'uniqueDialogue')
+path_dialogHashTable = os.path.join(path_uniqueDialog,'hashTable.txt')
 
 class textBoxOutline:
-    present_Left = 0
-    present_Right = 0
-    present_Top = 0
-    present_Right = 0
     present_All = 0
     present_Any = 0
     obstructed = 0
@@ -28,16 +30,12 @@ class textBoxOutline:
 
 tb_blueConv = textBoxOutline()
 
-tb_blueConv.path_Top = os.path.join(path_Border,'blue_Top')
-tb_blueConv.path_Bottom = os.path.join(path_Border,'blue_Bottom')
-tb_blueConv.path_Left = os.path.join(path_Border,'blue_Left')
-tb_blueConv.path_Right = os.path.join(path_Border,'blue_Right')
+tb_blueConv.path_Top = os.path.join(path_Border,'blue_Top.png')
+tb_blueConv.path_Bottom = os.path.join(path_Border,'blue_Bottom.png')
+tb_blueConv.path_Left = os.path.join(path_Border,'blue_Left.png')
+tb_blueConv.path_Right = os.path.join(path_Border,'blue_Right.png')
 
 def clearTextbox(outline):
-    outline.present_Left = 0
-    outline.present_Right = 0
-    outline.present_Top = 0
-    outline.present_Bottom = 0
     outline.present_All = 0
     outline.present_Any = 0
     outline.obstructed = 0
@@ -55,32 +53,87 @@ def crop_image(input_image, start_x, start_y, width, height):
     box = (start_x, start_y, start_x + width, start_y + height)
     return input_image.crop(box)
 
-def mainLoop():
-    
-    global firstScan
-    global prev_hash
-    
+def detect_tb(tbReference, searchImage):
     # Initialize text box object
-    clearTextbox(tb_blueConv)
+    clearTextbox(tbReference)
 
-    # Take screenshot
-    tb_Raw = pyautogui.screenshot(region=(40,760,1839,260)) # (region=(1960,760,1839,260))
-    #tb_Raw.save(path_tbRaw)
+    side = [0,0,0,0]
+    sideCount = 0
 
-    tb_textRaw = crop_image(tb_Raw, 88, 18, 1663, 224)
-    #tb_textRaw.save(path_tbTextRaw)
+    side[0] = pyautogui.locate(tbReference.path_Top, searchImage, grayscale=False)
+    side[1] = pyautogui.locate(tbReference.path_Bottom, searchImage, grayscale=False)
+    side[2] = pyautogui.locate(tbReference.path_Left, searchImage, grayscale=False)
+    side[3] = pyautogui.locate(tbReference.path_Right, searchImage, grayscale=False)
 
-    if firstScan:
-        prev_hash = imagehash.dhash(tb_textRaw)
-        firstScan = 0
-    new_hash = imagehash.dhash(tb_textRaw)
+    for x in side:
+        if not isinstance(x, type(None)):
+            tbReference.present_Any = 1
+            sideCount += 1
 
-    diff = new_hash - prev_hash
-    print(str(new_hash) + ", " + str(prev_hash) + ", diff = " + str(diff))
-    prev_hash = new_hash
+    # Check side presence
+    if not tbReference.present_Any:
+        tbReference.absent = 1
+    elif sideCount != 4:
+        tbReference.obstructed = 1
+    else:
+        tbReference.detected = 1
+
+uniqueHash = []
+hashDiffFlat_Count = 0
+appendNewHash = 0
 
 # MainProgram
 if __name__ == "__main__":
 
     while True:
-        mainLoop()
+
+        # Take screenshot
+        tb_Raw = pyautogui.screenshot(region=(40,760,1839,260)) # (region=(1960,760,1839,260))
+        #tb_Raw.save(path_tbRaw)
+
+        detect_tb(tb_blueConv, tb_Raw)
+
+        if tb_blueConv.detected:
+            tb_textRaw = crop_image(tb_Raw, 88, 18, 1663, 224)
+
+            if firstScan:
+                prev_hash = imagehash.dhash(tb_textRaw)
+                firstScan = 0
+
+            new_hash = imagehash.dhash(tb_textRaw)
+            diff = new_hash - prev_hash
+            print(str(new_hash) + ", " + str(prev_hash) + ", diff = " + str(diff))
+            prev_hash = new_hash
+
+            if abs(diff) >= 4:
+                hashDiffFlat_Count = 0
+            else:
+                hashDiffFlat_Count += 1
+
+            if hashDiffFlat_Count > 4:
+                appendNewHash = 1
+
+            for x in uniqueHash:
+                if abs(x - new_hash) < 4:
+                    appendNewHash = 0
+            
+            if appendNewHash:
+                uniqueHash.append(new_hash)
+
+                newIndex = len(uniqueHash) - 1
+
+                fileName = str(newIndex) + ".png"
+
+                path_newImage = os.path.join(path_uniqueDialog,fileName)
+                tb_textRaw.save(path_newImage)
+
+                with open(path_dialogHashTable, 'w') as fp:
+
+                    writeIndex = 0
+                    for x in uniqueHash:
+                        strvar = str(x)
+                        fp.write("%d, %s\n" % (writeIndex, strvar))
+                        writeIndex += 1
+                
+                engine.say('caught')
+                engine.runAndWait()
