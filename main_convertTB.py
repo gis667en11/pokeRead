f2a497078a2395e7f415676749b77ac76f52774f
gis667en11 <gis667en11@gmail.com>
2022-06-04 23:31:51 -0400

    Developed new hash practices

--- main_convertTB.py
+++ main_convertTB.py
@@ -1,23 +1,16 @@
 '''
-This program searches for sequentially named images in
-
-path.uniqueDialogue
-and
-path.screenshotFull
-
-If an image, by number, is missing from either, they are both deleted.
-If a gap in numbering is determined, subsequent image pairs are renamed to fill the gap.
+This program converts a previously created screenshot to a new
+hash reference image
 
 '''
 
 import os
-import sys
 import path
 from PIL import Image
 
 FINAL_IMAGE_INDEX = 1000
-FINAL_WIDTH = 554
-FINAL_HEIGHT = 525
+FINAL_WIDTH = 415
+FINAL_HEIGHT = 496
 
 def crop_image(input_image, start_x, start_y, width, height):
     """Pass input name image, output name image, x coordinate to start croping, y coordinate to start croping, width to crop, height to crop """
@@ -36,13 +29,10 @@ if __name__ == "__main__":
 
     while indexImage < FINAL_IMAGE_INDEX:
 
-        tb_ex = textBlock_exists(indexImage)
+        if textBlock_exists(indexImage):
 
-        # If both images exist, we have a good matching pair
-        if tb_ex:
-
-            fileName_old = str(indexImage) + ".png"
-            path_oldFile = os.path.join(path.uniqueDialog,fileName_old)
+            fileName_old = str(indexImage) + "_full.png"
+            path_oldFile = os.path.join(path.screenshotFull,fileName_old)
 
             imageOld = Image.open(path_oldFile)
 
@@ -53,22 +43,31 @@ if __name__ == "__main__":
             # testFileName[1] = str(indexImage) + "_test1.png"
             # testFileName[2] = str(indexImage) + "_test2.png"
 
-            for x in range(0,3):
+            oldImages.append(crop_image(imageOld, 128, 816, 1660, 62))
+            oldImages.append(crop_image(imageOld, 128, 909, 1660, 62))
+
+            newTB = Image.new('RGBA', (1660, 124))
+
+            newTB.paste(oldImages[0], (0,0))
+            newTB.paste(oldImages[1], (0,62))
+
+            tbChunks = []
+            for x in range(4):
                 # testImage = crop_image(imageOld, FINAL_WIDTH * x, 26, FINAL_WIDTH, 175)
                 # oldImages.append(testImage)
                 # path_testFile = os.path.join(path.tbForHash,testFileName[x])
                 # testImage.save(path_testFile,"PNG")
 
-                oldImages.append(crop_image(imageOld, FINAL_WIDTH * x, 26, FINAL_WIDTH, 175))
+                tbChunks.append(crop_image(newTB, FINAL_WIDTH * x, 0, FINAL_WIDTH, 124))
 
             new_im = Image.new('RGBA', (FINAL_WIDTH , FINAL_HEIGHT))
 
             y_offset = 0
-            for im in oldImages:
+            for im in tbChunks:
                 new_im.paste(im, (0,y_offset))
                 y_offset += im.size[1]
 
-            fileName_new = str(indexImage) + "_square.png"
+            fileName_new = str(indexImage) + ".png"
             path_newFile = os.path.join(path.tbForHash,fileName_new)
 
             new_im.save(path_newFile, "PNG")
